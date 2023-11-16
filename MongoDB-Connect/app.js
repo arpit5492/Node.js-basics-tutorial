70d48e7e72ef1ccf476932e4062f4e840465e5cf
arpit5492 <arpitmallick99@gmail.com>
2023-11-15 16:56:51 -0800

    Understood about adding data to database and sending data to browser

--- MongoDB-Connect/app.js
+++ MongoDB-Connect/app.js
@@ -5,30 +5,54 @@ const app = express();
 const morgan = require('morgan');
 // mongoose module
 const mongoose = require('mongoose');
+// Importing the model from /models/blog file
+const Blog = require('./models/blog');
 
 // connect to mongoDB
-const DBConn = 'mongodb+srv://arpitmallick99:arpit5492@cluster0.bk8r1ze.mongodb.net/?retryWrites=true&w=majority';
+const DBConn = 'mongodb+srv://arpitmallick99:arpit5492@cluster0.bk8r1ze.mongodb.net/BlogDB?retryWrites=true&w=majority';
 mongoose.connect(DBConn)
-  .then((result) => {console.log("Database is connected")})
+  .then((result) => {app.listen(3000)})
   .catch((err) => {console.log(err)});
 
-// register view engine
-app.set("view engine", "ejs"); // By default express and ejs will look into the views folder
-// But we can also set a different folder like below:
-// app.set("views", "folder name");
-
-// app.use((req, res, next)=>{
-//     console.log("New request made");
-//     console.log("host: ", req.hostname);
-//     console.log('path: ', req.path);
-//     console.log("method: ", req.method);
-//     next();
-// });
-// 3rd party middleware
+app.set("view engine", "ejs");
 app.use(morgan('dev'));
+app.use(express.static('public')); 
 
-// Middleware & static files
-app.use(express.static('public')); // Making the files public to show in the server
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) =>{
+  const blog = new Blog({
+    title: 'Blog 2',
+    snippet: 'I have a pet dog named Kimi',
+    body: 'Welcome to my second blog'
+  });
+  blog.save()
+    .then((result) =>{
+      res.send(result);
+    })
+    .catch((err) =>{
+      console.log(err);
+    })
+})
+
+app.get('/all-blogs', (req, res)=>{
+  Blog.find()
+    .then((result) =>{
+      res.send(result);
+    })
+    .catch(err =>{
+      throw(err);
+    });
+});
+
+app.get('/single-blog', (req, res)=>{
+  Blog.findById('655565569130761650a6e58c')
+    .then((result) => {
+      res.send(result);
+    })
+    .catch(err=>{
+      console.log(err);
+    });
+});
 
 app.get('/', function(req, res){
     // console.log(req.url, req.method);
@@ -61,23 +85,12 @@ app.get('/about', function(req, res){
     res.render('about', {title: 'About'});
 });
 
-// app.use((req, res, next)=>{
-//     console.log("In the next middleware");
-//     next();
-// });
-
 app.get("/blogs/create", (req, res)=>{
     res.render('create', {title: 'Create a blog'});
 });
 
-// 404 error page
 app.use(function(req, res){
     res.status(404).render('error', {title: '404 error'});
 });
 
-// listen for requests
 
-app.listen(3000, (err)=>{ // By default it's set to "localhost"
-    if(err) throw err;
-    console.log("Listening for requests from the client side");
-});
