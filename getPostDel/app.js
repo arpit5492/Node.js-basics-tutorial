5642fa4696262dcfd79b366b7d8b43054634f33a
arpit5492 <arpitmallick99@gmail.com>
2023-11-16 01:03:03 -0800

    New folder added

---
+++ getPostDel/app.js
@@ -0,0 +1,51 @@
+// express app
+const express = require('express');
+const app = express();
+// 3rd party middleware app(morgan)
+const morgan = require('morgan');
+// mongoose module
+const mongoose = require('mongoose');
+// Importing the model from /models/blog file
+const Blog = require('./models/blog');
+// Getting the username and password of the database from another file
+const pass = require('./.password');
+
+// connect to mongoDB
+const DBConn = 'mongodb+srv://'+pass.USER+':'+pass.PASSWORD+'@cluster0.bk8r1ze.mongodb.net/'+pass.DB_NAME+'?retryWrites=true&w=majority';
+mongoose.connect(DBConn)
+  .then((result) => {app.listen(3000)})
+  .catch((err) => {console.log(err)});
+
+app.set("view engine", "ejs");
+app.use(morgan('dev'));
+app.use(express.static('public')); 
+
+app.get('/', function(req, res){
+    // console.log(req.url, req.method);
+    res.redirect('/All-blogs');
+});
+
+// Outputting documents in views
+app.get('/All-blogs', (req, res)=>{
+  Blog.find().sort({ createdAt: -1})
+    .then((result)=>{
+      res.render('index', {title: 'All Blogs', b: result});
+    })
+    .catch(err =>{
+      console.log(err);
+    });
+});
+
+app.get('/about', function(req, res){
+    res.render('about', {title: 'About'});
+});
+
+app.get("/blogs/create", (req, res)=>{
+    res.render('create', {title: 'Create a blog'});
+});
+
+app.use(function(req, res){
+    res.status(404).render('error', {title: '404 error'});
+});
+
+
