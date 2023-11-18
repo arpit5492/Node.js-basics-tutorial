076df0b5bbfe6ca132c28e323094a91dce9d059c
arpit5492 <arpitmallick99@gmail.com>
2023-11-18 01:26:17 -0800

    Updated the code with MVC approach

---
+++ ExpressRouter/controller/blogController.js
@@ -0,0 +1,55 @@
+const Blog = require('../models/blog');
+
+const blogIndex = (req, res) => {
+    Blog.find().sort({ createdAt: -1}) // sorting in descending order
+      .then((result)=>{
+        res.render('blogs/index', {title: 'All Blogs', b: result});
+      })
+      .catch(err =>{
+        console.log(err);
+    });
+};
+
+const blogAdd = (req, res) => {
+    const blog = new Blog(req.body);
+    blog.save()
+      .then((result)=>{
+        res.redirect('/blogs');
+      })
+      .catch(err=>{
+        console.log(err);
+    });
+};
+
+const blogDetails = (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+      .then((result)=>{
+        res.render('blogs/blog-details', {title: 'Blog Details', blog: result});
+      })
+      .catch(err=>{
+        console.log(err);
+    });
+};
+
+const blogDel = (req, res) => {
+    const id = req.params.id;
+  
+    Blog.findByIdAndDelete(id)
+      .then((result) => {
+        res.json({redirect: '/blogs'});
+      })
+      .catch(err => console.log(err));
+};
+
+const blogCreate = (req, res) => {
+    res.render('blogs/create', {title: 'Create a blog'});
+};
+
+module.exports = {
+    blogIndex,
+    blogAdd,
+    blogDetails,
+    blogDel,
+    blogCreate
+};
--- ExpressRouter/router/blogRoutes.js
+++ ExpressRouter/router/blogRoutes.js
@@ -1,59 +1,23 @@
-const Blog = require('../models/blog');
-
 const express = require('express');
 const routes = express();
 
+const blogController = require('../controller/blogController');
+
 
 // Outputting documents in views
-routes.get('/', (req, res)=>{
-    Blog.find().sort({ createdAt: -1}) // sorting in descending order
-      .then((result)=>{
-        res.render('index', {title: 'All Blogs', b: result});
-      })
-      .catch(err =>{
-        console.log(err);
-      });
-  });
+routes.get('/', blogController.blogIndex);
   
   // Adding a new blog and then redirecting to the home page
-  routes.post('/', (req, res)=>{
-    // console.log(req.body);
-    const blog = new Blog(req.body);
-    blog.save()
-      .then((result)=>{
-        res.redirect('/blogs');
-      })
-      .catch(err=>{
-        console.log(err);
-      });
-  });
+  routes.post('/', blogController.blogAdd);
   
-  routes.get("/create", (req, res)=>{
-    res.render('create', {title: 'Create a blog'});
-  });
+  routes.get("/create", blogController.blogCreate);
   
   // Getting a single blog
-  routes.get('/:id', (req, res)=>{
-    const id = req.params.id;
-    Blog.findById(id)
-      .then((result)=>{
-        res.render('blog-details', {title: 'Blog Details', blog: result});
-      })
-      .catch(err=>{
-        console.log(err);
-      });
-  });
+  routes.get('/:id', blogController.blogDetails);
   
   // Deleting a blog from the database and showing the remaining blogs after deleting
-  routes.delete("/:id", (req, res)=>{ // As this is an ajax request, so we can't use redirect method here
+  routes.delete("/:id", blogController.blogDel); // As this is an ajax request, so we can't use redirect method here
   // That's why we will be sending a json to the frontend part, and then from the frontend we will redirect to the home page
-    const id = req.params.id;
-  
-    Blog.findByIdAndDelete(id)
-      .then((result) => {
-        res.json({redirect: '/blogs'});
-      })
-      .catch(err => console.log(err));
-  });
+
 
   module.exports = routes;
